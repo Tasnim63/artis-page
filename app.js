4d9553a38988ab6c4612dbf40235739b6f95fa75
tasnim63 <tamanna202163@gmail.com>
2022-03-08 10:37:57 +0600

    appenchild comit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+
+
 const elementById = (id) => {
-    document.getElementById(id);
+  return  document.getElementById(id);
   };
   
   const handleSearch = () => {
@@ -7,31 +9,42 @@ const elementById = (id) => {
     const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => showArtists(data));
-  };
+      .then((data) =>showArtists(data));
+};
+const showArtists = ({artists}) => {
+  // console.log(data);
+  const artistsContainer = elementById('artists');
+  artists.forEach(artist => {
+    // console.log(artist);
+    const div = document.createElement("div");
+    div.classList.add("artist-card");
+    div.innerHTML = `<div class="image-container">
+    <div class="image-container-inner">
+      <img
+        src="${artist.strArtistThumb}"
+        alt=""
+      />
+    </div>
+  </div>
+  <div class="info-container">
+    <h1>${artist.strArtist}</h1>
+    <p>Country: ${artist.strCountry}</p>
+    <p>Style: ${artist.strGenre}</p>
+  </div>
+  <button class="album-button">
+    <i class="fa-solid fa-compact-disc"></i>
+    <p onclick="fetchAlbums('${artist.idArtist}')" class="button-title">Albums</p>
+  </button>`;
+    artistsContainer.appendChild(div);
+  });
+  }
   
-  const showArtists = (data) => {
-    const artistContainer = elementById("artist");
-    data?.artists?.forEach((artist) => {
-      const div = document.createElement("div");
-      div.classList.add("artist-card");
-      div.innerHTML = `<div class="image-container">
-      <div class="image-container-inner">
-        <img
-          src="${artist.strArtistThumb}"
-          alt=""
-        />
-      </div>
-    </div>
-    <div class="info-container">
-      <h1>${artist.strArtist}</h1>
-      <p>Country: ${artist.strCountry}</p>
-      <p>Style: ${artist.strGenre}</p>
-    </div>
-    <button class="album-button">
-      <i class="fa-solid fa-compact-disc"></i>
-      <p onclick="fetchAlbums('${artist.idArtist}')" class="button-title">Albums</p>
-    </button>`;
-      artistContainer.appendChild(div);
-    });
-  };
+// const showArtists = (data) => {
+//     console.log(data);
+//     const artistContainer = elementById("artists");
+//     data?.artists?.forEach((artist) => {
+    
+//       artistContainer.appendChild(div);
+//     });
+//   };
+  
